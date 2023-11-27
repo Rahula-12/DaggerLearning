fcc064801200cfd7201f93d47f460bc56e64ec29
rahul <ra9853044@gmail.com>
2023-11-27 23:02:11 +0530

    added singleton annotation to UserRepositoryModule's sqlrepository method

--- app/src/main/java/com/example/daggerlearning/UserRepositoryModule.kt
+++ app/src/main/java/com/example/daggerlearning/UserRepositoryModule.kt
@@ -3,11 +3,13 @@ package com.example.daggerlearning
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 abstract class UserRepositoryModule{
 
     @Binds
+    @Singleton
     abstract fun getSQLRepository(sqlRepository: SQLRepository):UserRepository
 
 //    @Provides
