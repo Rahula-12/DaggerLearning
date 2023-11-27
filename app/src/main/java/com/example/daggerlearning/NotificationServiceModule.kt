aa4e56834123ed5ab17a45a565f469aa1e731484
rahul <ra9853044@gmail.com>
2023-11-27 22:58:13 +0530

    added singleton annotation to NotificationServiceModule's messageService method

--- app/src/main/java/com/example/daggerlearning/NotificationServiceModule.kt
+++ app/src/main/java/com/example/daggerlearning/NotificationServiceModule.kt
@@ -4,6 +4,7 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 class NotificationServiceModule {
@@ -12,6 +13,7 @@ class NotificationServiceModule {
     @Provides
     fun getEmailService(emailService: EmailService):NotificationService=emailService
 
+    @Singleton // to return singleton object of messageService
     @MessageQualifier
     @Provides
     fun getMessageService(retryCount:Int):NotificationService=MessageService(retryCount)
