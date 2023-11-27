f8cf0a73355fa2a6c2777d69f02d33fdfa54acca
rahul <ra9853044@gmail.com>
2023-11-27 22:54:43 +0530

    added singleton annotation on emailservice class

--- app/src/main/java/com/example/daggerlearning/EmailService.kt
+++ app/src/main/java/com/example/daggerlearning/EmailService.kt
@@ -2,10 +2,12 @@ package com.example.daggerlearning
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
 interface NotificationService{
     fun send(to: String, body: String)
 }
+@Singleton
 class EmailService @Inject constructor() : NotificationService{
     override fun send(to: String, body: String) {
         Log.d("DI", "Email $body sent to $to.")
--- app/src/main/java/com/example/daggerlearning/MainActivity.kt
+++ app/src/main/java/com/example/daggerlearning/MainActivity.kt
@@ -9,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var userRegistrationService: UserRegistrationService
 
+    @Inject
+    lateinit var emailService: EmailService
+
+    @Inject
+    lateinit var emailService2: EmailService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
